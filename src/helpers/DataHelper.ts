ad9091e1dde52625d4b3ed6222acf2b256925094
Andrei Gaspar <andrei@planeeet.com>
2019-02-06 19:00:15 +0200

    - Extended data helper populate function

--- src/helpers/DataHelper.ts
+++ src/helpers/DataHelper.ts
@@ -46,37 +46,124 @@ class DataHelper {
         });
 
 
-        const memberJohn = new Member({
-            name: "John",
+        const memberStephen = new Member({
+            name: "Stephen Hodges",
             owner: andrei
         });
 
 
-        const noteJohn1 = new Note({
+        const noteStephen1 = new Note({
             owner: andrei,
-            member: memberJohn,
+            member: memberStephen,
             content: "John note 1"
         });
 
 
-        const noteJohn2 = new Note({
+        const noteStephen2 = new Note({
             owner: andrei,
-            member: memberJohn,
+            member: memberStephen,
             content: "John note 2"
         });
 
-        memberJohn.teams.push( team1._id );
-        memberJohn.notes.push( noteJohn1._id );
-        memberJohn.notes.push( noteJohn2._id );
+        memberStephen.teams.push( team1._id );
+        memberStephen.notes.push( noteStephen1._id );
+        memberStephen.notes.push( noteStephen2._id );
 
+
+        const memberLee = new Member({
+            name: "Lee Simon",
+            owner: andrei
+        });
+
+        memberLee.teams.push( team1._id );
+
+
+        const memberVictoria = new Member({
+            name: "Victoria Terry",
+            owner: andrei
+        });
+
+        memberVictoria.teams.push( team1._id );
+
+
+        const memberEstelle = new Member({
+            name: "Estelle Cruz",
+            owner: andrei
+        });
+
+        memberEstelle.teams.push( team1._id );
+
+
+        const memberSteve = new Member({
+            name: "Steve Cannon",
+            owner: andrei
+        });
+
+        memberSteve.teams.push( team1._id );
+
+
+        const memberGordon = new Member({
+            name: "Gordon Hunt",
+            owner: andrei
+        });
+
+        memberGordon.teams.push( team1._id );
+
+
+        const memberCharles = new Member({
+            name: "Charles Hughes",
+            owner: andrei
+        });
+
+        memberCharles.teams.push( team1._id );
+
+
+        const memberIsabel = new Member({
+            name: "Isabel Estrada",
+            owner: andrei
+        });
+
+        memberIsabel.teams.push( team1._id );
+
+
+        const team2 = new Team({
+            name: "Echipa Racheta",
+            owner: andrei
+        });
+
+        const memberChiki = new Member({
+            name: "Chiki Chan",
+            owner: andrei
+        });
+
+        memberChiki.teams.push( team2._id );
+
+        const memberJocka = new Member({
+            name: "Jocka Mester",
+            owner: andrei
+        });
+
+        memberJocka.teams.push( team2._id );
 
         Promise.all([
             andrei.save(),
             team1.save(),
-            memberJohn.save(),
-            noteJohn1.save(),
-            noteJohn2.save()
+            team2.save(),
+            memberStephen.save(),
+            noteStephen1.save(),
+            noteStephen2.save(),
+            memberLee.save(),
+            memberVictoria.save(),
+            memberEstelle.save(),
+            memberSteve.save()
         ])
+            .then( () => Promise.all([
+                        memberGordon.save(),
+                        memberCharles.save(),
+                        memberIsabel.save(),
+                        memberChiki.save(),
+                        memberJocka.save()
+                ]))
             .then( () => res.send( "Database successfully populated." ) )
             .catch( next );
 
