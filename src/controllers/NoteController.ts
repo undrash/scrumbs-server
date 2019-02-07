9c3182d328e6975509f61b6b65b3a2378cec2a7e
Andrei Gaspar <andrei@planeeet.com>
2019-02-07 22:58:04 +0200

    - Note controller get notes, solve and unsolve impediments

--- src/controllers/NoteController.ts
+++ src/controllers/NoteController.ts
@@ -1,6 +1,7 @@
 
 
 import { Router, Request, Response, NextFunction } from "express";
+import Note from "../models/Note";
 
 
 
@@ -18,14 +19,42 @@ class NoteController {
 
 
     public routes() {
-
+        this.router.get( "/:member", this.getNotes );
+        this.router.put( "/solve/:id", this.solve );
+        this.router.put( "/unsolve/:id", this.unsolve );
     }
 
 
 
+    public getNotes(req: Request, res: Response, next: NextFunction) {
+        const { member } = req.params;
+
+        Note.find( { member } )
+            .sort( { date: -1 } )
+            .then( notes => res.status( 200 ).json( { success: true, notes } ) )
+            .catch( next );
+    }
 
 
 
+    public solve(req: Request, res: Response, next: NextFunction) {
+        const { id } = req.params;
+
+        Note.findByIdAndUpdate( id, { isSolved: true }, { "new" : true } )
+            .then( note => res.status( 200 ).json( { success: true, note } ) )
+            .catch( next );
+    }
+
+
+
+    public unsolve(req: Request, res: Response, next: NextFunction) {
+        const { id } = req.params;
+
+        Note.findByIdAndUpdate( id, { isSolved: false }, { "new" : true } )
+            .then( note => res.status( 200 ).json( { success: true, note } ) )
+            .catch( next );
+    }
+    
 }
 
 
