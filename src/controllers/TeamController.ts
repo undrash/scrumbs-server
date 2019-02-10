0bf3a20015f6b837f8dc5992462695af562a1a55
Andrei Gaspar <andrei@planeeet.com>
2019-02-10 21:43:25 +0200

    - Update team route

--- src/controllers/TeamController.ts
+++ src/controllers/TeamController.ts
@@ -23,6 +23,7 @@ class TeamController {
     public routes() {
         this.router.get( '/', this.getTeams );
         this.router.post( '/', this.createTeam );
+        this.router.put( '/', this.updateTeam );
     }
 
 
@@ -64,7 +65,46 @@ class TeamController {
             .then( members => res.status( 200 ).json( { success: true, team, members } ) )
             .catch( next );
 
-    }
+    };
+
+
+
+    public updateTeam = async (req: Request, res: Response, next: NextFunction) => {
+        const { id, name, members } = req.body;
+
+        if ( ! id || ! name ) {
+            res.status( 422 ).json( { success: false, message: "Invalid arguments provided for team update." } );
+            return;
+        }
+
+        let addedMembers: string[]      = [];
+        let removedMembers: string[]    = [];
+
+        if ( members ) {
+            const currentMembers    = await Member.find( { teams: { $in: id } } );
+            const currentMemberIds  = currentMembers.map( m => (m as any)._id.toString() );
+
+            addedMembers            = members.filter( (m: any) => currentMemberIds.indexOf( m ) === -1 );
+            removedMembers          = currentMemberIds.filter( (m: any) => members.indexOf( m ) === -1 );
+
+
+            await Promise.all([
+                Member.update(
+                    { _id: { $in: addedMembers } },
+                    {  $push: { teams: id } },
+                    { multi: true } ),
+
+                Member.update(
+                    { _id: { $in: removedMembers } },
+                    {  $pull: { teams: id } },
+                    { multi: true } )
+            ]);
+        }
+
+        Team.findByIdAndUpdate( id, { name }, { "new" : true } )
+            .then( team => res.status( 200 ).json( { success: true, team, addedMembers, removedMembers } ) )
+            .catch( next );
+    };
 
 
 
